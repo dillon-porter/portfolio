3eea77a5ec1aaeed0de214dca6a204077bd8f9c2
Dillon Porter <dillonporter@hotmail.com>
2023-08-02 18:02:46 -0230

    updates to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -54,7 +54,7 @@ const Footer = () => {
             </a>
         </div>
         <p className="mt-8 text-base leading-6 text-center text-gray-400">
-            © 2023 Dillon Porter's Portfolio.
+            © 2023 Dillon's Portfolio.
         </p>
     </div>
         </div>
